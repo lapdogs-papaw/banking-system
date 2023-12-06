7fc6b12479b32ab867b38439bf3775e142a532d4
Greg W <152864600+lapdogs-papaw@users.noreply.github.com>
2023-12-06 11:01:59 +0600

    Create BankSystem.cpp

---
+++ BankSystem.cpp
@@ -0,0 +1,19 @@
+// BankSystem.cpp
+#include "BankSystem.h"
+#include <iostream>
+
+void BankSystem::run() {
+    // Your main banking system logic goes here
+}
+
+void BankSystem::createCustomer(const std::string& name) {
+    // Logic to create a new customer
+}
+
+void BankSystem::openAccount(int customerId, double initialBalance) {
+    // Logic to open a new account for a customer
+}
+
+void BankSystem::displayCustomerInfo(int customerId) const {
+    // Logic to display customer information
+}
